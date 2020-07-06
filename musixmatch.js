9094963cb47a5c4f726674d0e2e43916ba1672f2
Andrea Virgen <andrealejandra@andreas-mbp.lan>
2020-07-06 09:15:17 -0500

    show music favorites on favorites page, spotify changes, musixmatch edits

--- musixmatch.js
+++ musixmatch.js
@@ -22,19 +22,23 @@ $("#searchbtn").on("click",function(event){
     event.preventDefault();
     var searchSong = $("#searchbox").val().trim();
     $("#searchbox").val(" ");
-    search();
-  
+    search(searchSong);
+    resultsDiv.append($("<button>").addClass("btn").attr("id","redobtn").text("Search Again"));     
+
+});
 
 //this function should only be triggered when the search button is pressed
-function search(){
+function search(searchSong){
     event.preventDefault();
+    resultsDiv.empty();
+  
 
 $.ajax({
     type: "GET",
     data: {
         apikey:"200a4593b9277ce9ffb162e74cb71ea0",
         q_track: searchSong,
-        page_size: 1,
+        page_size: 5,
         format:"jsonp",
         callback:"jsonp_callback"
     },
@@ -45,9 +49,17 @@ $.ajax({
     success: function(data) {
         console.log(data);
         console.log(data.message.body.track_list[0].track.track_name);
-        // var song = `data.message.body.track_list[0].track.track_name`;
-        // var artist = `data.message.body.track_list[0].track.artist_name`;
-        $("#resultsDiv").html(`<h5>Search Result</h5><br> Song:${data.message.body.track_list[0].track.track_name} <br>Artist:${data.message.body.track_list[0].track.artist_name}`);
+        
+        var song = data.message.body.track_list[0].track.track_name;
+        var artist = data.message.body.track_list[0].track.artist_name;
+
+        //add search again button, append ajax result, and add favorite button
+        resultsDiv.append($("<p>").text("Song:" + song));
+        resultsDiv.append($("<p>").text("Artist:" + artist));
+
+        //add favorites button
+        resultsDiv.append($("<button>").addClass("btn").attr("id","favTrack").text("Favorite"));     
+
     },
     error: function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR);
@@ -56,32 +68,48 @@ $.ajax({
     }  
     
 });
+    //add to local storage -- not working
+    $("#favTrack").on("click",function(event){
+        console.log("favorites clicked!");
+    
+        resultsDiv.append($("<p>").text("Added to favorites!"));
+        console.log(song + artist );
+        localStorage.setItem("songandartist",song + artist );
 
+    });
 };
-});
 //random lyrics to guide search parameters
-randomLyricArray = ["love","loved","waiting for you","date","beautiful","two of us","mine","crazy about you","in love"];
+randomLyricArray = ["love","loved","waiting for you","date","lovin'","forever","yours","beautiful","two of us","mine","crazy about you","in love"];
 
-// pulls random lyric from array
-var randomLyric = randomLyricArray[Math.floor(Math.random() * randomLyricArray.length)];
 
 //console.log(randomLyric);
 
 
 $("#random").on("click", (function(event){
-    event.preventDefault();
-    random();
+    resultsDiv.empty();
+    
+    for ( var i = 0; i < 6 ; i++) {
+        event.preventDefault();
+        randomPlaylist();
+
+};
 }));
 
-//ajax call with the parameters of amount of tracks (5) with each song containing a random lyric from the randomLyric array
-function random(){
+
+
+//ajax call with the parameters of a random lyric from the randomLyric array
+function randomPlaylist(){
+
+// pulls random lyric from array
+var randomLyric = randomLyricArray[Math.floor(Math.random() * randomLyricArray.length)];
+
 
     $.ajax({
       type: "GET",
       data: {
           apikey:"200a4593b9277ce9ffb162e74cb71ea0",
           q_lyrics:randomLyric,
-          page_size: 5,
+          page_size: 1,
           format:"jsonp",
           callback:"jsonp_callback"
       },
@@ -91,27 +119,35 @@ function random(){
       contentType: 'application/json',
       success: function(data) {
           //console.log(data); 
-          console.log(data.message.body.track_list);
-          var trackList = data.message.body.track_list;
-
-          var songName = [];
-          var artistName = [];
-
-          for (var i = 0; i < trackList.length; i++){
-              console.log(i);
+          //console.log(data.message.body.track_list);
            
-            songName [i]= data.message.body.track_list[i].track.track_name
-            artistName [i]= data.message.body.track_list[i].track.artist_name
-    $("#resultsDiv").append($("<p>").text("Song:" + songName));
-    $("#resultsDiv").append($("<p>").text("Artist:" + artistName))
-};
-//calls for loop function to make list of songs
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR);
-          console.log(textStatus);
-          console.log(errorThrown);
-      }
+        songName = data.message.body.track_list[0].track.track_name;
+        artistName = data.message.body.track_list[0].track.artist_name;
+
+    resultsDiv.append($("<p>").text("Song:" + songName));
+    resultsDiv.append($("<p>").text("Artist:" + artistName));
+    resultsDiv.append($("<button>").addClass("btn favTrack").text("Favorite"));
+},
+
+
+    //   error: function(jqXHR, textStatus, errorThrown) {
+    //       console.log(jqXHR);
+    //       console.log(textStatus);
+    //       console.log(errorThrown);
+    //   }
         
-      })
-    }
+      });
+
+//add to local storage
+$(".favTrack").on("click",function(event){
+    console.log("favorites clicked!");
+
+    resultsDiv.append($("<p>").text("Added to favorites!"));
+    console.log(songName + artistName );
+    localStorage.setItem("songandartist",songName + artistName );
+})
+
+    };
+
+
+
